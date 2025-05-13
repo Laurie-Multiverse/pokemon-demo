1b8face9fca0e7c3411d10331edb838df03d1227
Laurie-Multiverse <laurie.corrin@multiverse.io>
2025-05-13 09:39:16 -0700

    completed SQL demo

--- pokemon.sql
+++ pokemon.sql
@@ -1,46 +1,49 @@
--- CREATE 
--- Create the table of pokemon
--- CREATE TABLE IF NOT EXISTS pokemon (
---     id INTEGER PRIMARY KEY,
---     name TEXT,
---     type TEXT,
---     weight INTEGER
---     );
+-- CREATE (C in CRUD)
+CREATE TABLE IF NOT EXISTS pokemon(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    type TEXT,
+    weight INTEGER
+);
 
--- INSERT INTO pokemon (name, type, weight)
--- VALUES
---     ('Pikachu', 'Electric', 13),
---     ('Charmander', 'Fire', 19),
---     ('Bulbasaur', 'Grass', 15)
--- ;
+-- CREATE/INSERT rows
+INSERT INTO pokemon(name, type, weight)
+VALUES
+  ('Pikachu', 'Electric', 13),
+  ('Charmander', 'Fire', 19),
+  ('Bulbasaur', 'Grass', 15)    
+;
 
--- -- UPDATE
--- UPDATE pokemon
--- SET weight=10
--- WHERE id=1;
-
--- UPDATE pokemon
--- SET 
---     name="Charmeleon",
---     weight=50
--- WHERE id=2;
+INSERT INTO pokemon (name, type, weight)
+VALUES ("Squirtle", "water", 12);
 
 
--- -- DELETE
--- DELETE FROM pokemon
--- WHERE id=3;
+-- UPDATE (U from CRUD)
+UPDATE pokemon
+SET weight=10
+WHERE id=1;
 
--- SELECT *
--- FROM pokemon;
+UPDATE pokemon
+SET
+    name="Charmeleon",
+    weight=50
+WHERE id=2;
 
--- INSERT INTO pokemon (name, type, weight)
--- VALUES ("Squirtle", "water", 12);
+-- DELETE (D from CRUD)
+DELETE FROM pokemon
+WHERE id=1;
 
--- SELECT *
--- FROM pokemon;
+-- READ (R from CRUD)
+SELECT *
+FROM pokemon;
+
+SELECT name, type
+FROM pokemon;
 
 -- DELETE THE TABLE *SCARY*
 DROP TABLE IF EXISTS pokemon;
+
+-- Showing two tables and associating then
 
 CREATE TABLE pokemon(
     id INTEGER PRIMARY KEY,
@@ -63,28 +66,32 @@ VALUES
 
 SELECT * FROM decks;
 
-INSERT INTO pokemon (name, type, weight, deck_id)
-VALUES
+INSERT INTO pokemon(name, type, weight, deck_id)
+VALUES 
   ('Pikachu', 'lightning', 13, 1),
   ('Squirtle', 'water', 20, 2),
   ('Charmander', 'fire', 19, 1),
   ('Magikarp', 'water', 11, null)
-  ;
+;
 
-  SELECT * FROM pokemon;
+SELECT * 
+FROM pokemon;
 
-  -- READ
-  SELECT name, type
-  FROM pokemon;
+-- see ALL pokemon with their decks, ALL columns
+SELECT *
+FROM pokemon
+LEFT JOIN decks
+ON pokemon.deck_id=decks.id;
 
--- show ALL pokemon and their decks in one table
-  SELECT pokemon.name, type, decks.title
-  FROM pokemon
-  LEFT JOIN decks
-  ON pokemon.deck_id = decks.id;
+-- see ALL pokemon with their decks, specified info
+SELECT pokemon.name, pokemon.type, decks.title
+FROM pokemon
+LEFT JOIN decks
+ON pokemon.deck_id=decks.id;
 
--- show ALL pokemon in decks and their decks in one table
-  SELECT pokemon.name, type, decks.title
-  FROM pokemon
-  INNER JOIN decks
-  ON pokemon.deck_id = decks.id;
+-- see only pokemon who have decks, specified info
+SELECT pokemon.name, pokemon.type, decks.title
+FROM pokemon
+INNER JOIN decks
+ON pokemon.deck_id=decks.id;
+
