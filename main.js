e7b2ebd0d44a8bf19527254f2b584e3f436db168
Laurie Corrin <laurie.corrin@multiverse.io>
2025-05-14 13:54:46 +0000

    update day1 with comments

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
+/*
+    Demonstrate async JS using file commands
+    - read/write file operations are asynchronous because they can take a long time
+*/
+
+// first, require the 'promises' version of the file system functions
 const { readFile, writeFile } = require ('fs').promises;
+
+// Create a file path (OS-independent) to our pokemon.json file in the current directory
 const path = require('path');
 const POKEMON_FILE_PATH = path.join(__dirname, "pokemon.json");
 
 async function main() {
+    // First, let's work with our pokemon in memory. 
+    // We can do CRUD operations on an array of pokemon
+    // But, when we restart the app, that data will be gone
     const pikachu = {
         name: "Pikachu",
         type: "Electric"
@@ -18,7 +29,7 @@ async function main() {
     pokemonArray.push(charizard);
     console.log("My Pokemon (in array):" , pokemonArray)
 
-    // Use a file to persist our pokemon
+    // Now, let's use a file to save our pokemon and reload them when we run the app again
     const buffer = await readFile(POKEMON_FILE_PATH);
     const pokemonDB = JSON.parse(buffer); // vs. res.json()
     console.log('Loaded Pokemon:', pokemonDB);
